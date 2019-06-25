1d5347726d5f20a961a2b1d9598546b0b4c43371
Marcos Aquino <marcos-aquino@live.com>
2019-06-24 22:00:54 -0300

    Classe item criada

---
+++ src/main/java/trb3/dcc193/trb3/Item.java
@@ -0,0 +1,58 @@
+import java.util.List;
+
+public class Item {
+
+    private Long id;
+    private String titulo;
+    private List<Anotacao> anotacoes;
+    private List<Etiqueta> etiquetas;
+    private List<Vinculo> vinculos;
+
+    public Item(){
+
+    }
+
+    public Item(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Anotacao> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(List<Anotacao> anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+
+    public List<Etiqueta> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEtiquetas(List<Etiqueta> etiquetas) {
+        this.etiquetas = etiquetas;
+    }
+
+    public List<Vinculo> getVinculos() {
+        return vinculos;
+    }
+
+    public void setVinculos(List<Vinculo> vinculos) {
+        this.vinculos = vinculos;
+    }
+}
