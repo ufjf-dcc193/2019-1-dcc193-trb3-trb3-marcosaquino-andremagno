0f57134c08623be9000f7a12570f54d5254f5b36
Marcos Aquino <marcos-aquino@live.com>
2019-06-24 22:14:07 -0300

    Classe Usuario criada

---
+++ src/main/java/trb3/dcc193/trb3/Usuario.java
@@ -0,0 +1,62 @@
+
+
+public class Usuario{
+
+
+    private Long id;
+    private String nomeCompleto;
+    private int codigo;
+    private String descricao;
+    private String email;
+
+    public Usuario(){
+
+    }
+
+    public Usuario(String nomeCompleto, int codigo, String descricao, String email) {
+        this.nomeCompleto = nomeCompleto;
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }    
+}
