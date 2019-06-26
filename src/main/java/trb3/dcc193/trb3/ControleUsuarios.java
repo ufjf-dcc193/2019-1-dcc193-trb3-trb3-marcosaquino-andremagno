1be0972d75f60e87e7a70b54fd27bf65db3ffd7b
Marcos Aquino <marcos-aquino@live.com>
2019-06-26 12:21:10 -0300

    Implementando cadastro de usuarios

--- src/main/java/trb3/dcc193/trb3/ControleUsuarios.java
+++ src/main/java/trb3/dcc193/trb3/ControleUsuarios.java
@@ -1,5 +1,6 @@
 package trb3.dcc193.trb3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,11 +8,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ControleUsuarios{
 
+    @Autowired
+    private RepositorioUsuario repositorioUsuario;
+
     @RequestMapping(method = RequestMethod.GET, value = "/cadastrousuario")
     public String inicio(){
 
 
         return "cadastro/cadastrousuario";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/salvarusuario")
+    public String salvar(Usuario usuario){
+
+        repositorioUsuario.save(usuario);
+        return "cadastro/cadastrousuario";
+
+
     }
 
 }
