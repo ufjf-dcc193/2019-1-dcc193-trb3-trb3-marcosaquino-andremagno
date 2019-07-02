20a061de756305c11054b991d82d00c55ff30c21
andremagnoribeiro <andremagnoribeiro@gmail.com>
2019-07-01 23:24:43 -0300

    Cadastro de Anotações Pronto

--- src/main/java/trb3/dcc193/trb3/ControllerAnotacao.java
+++ src/main/java/trb3/dcc193/trb3/ControllerAnotacao.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * ControllerAnotacao
  */
 @Controller
+
 public class ControllerAnotacao {
 
     @Autowired
@@ -22,29 +23,31 @@ public class ControllerAnotacao {
 
     @RequestMapping("anotacao")
     public String listarAnotacao(Model model){
+       
         model.addAttribute("anotacoes",repositorioAnotacao.findAll());
         return "anotacao/listar";
     }
     
-    @RequestMapping("/criar")
-    public String criarAnotacao(@PathVariable Long id){
-        repositorioAnotacao.deleteById(id);
-        return "redirect:/anotacao";
+    @RequestMapping("anotacao/criar")
+    public String criarAnotacao(Model model){
+        model.addAttribute("anotacao", new Anotacao());
+        model.addAttribute("usuarios",repositorioUsuario.findAll());
+        return "anotacao/criar";
     }
 
-    @RequestMapping("/deletar/{id}")
+    @RequestMapping("anotacao/deletar/{id}")
     public String deletarAnotacao(@PathVariable Long id){
         repositorioAnotacao.deleteById(id);
         return "redirect:/anotacao";
     }
 
-    @RequestMapping("/editar/{id}")
+    @RequestMapping("anotacao/editar/{id}")
     public String editarAnotacao(@PathVariable Long id, Model model){
         model.addAttribute("anotacao",repositorioAnotacao.findById(id).get());
         return "anotacao/editar";
     }
 
-    @RequestMapping("/editar/salvar")
+    @RequestMapping("anotacao/editar/salvar")
     public String editarSalvarAnotacao(Anotacao anotacao){
         Date data=new Date();
         anotacao.setDataDeAlteracao(data.toString());
@@ -52,7 +55,7 @@ public class ControllerAnotacao {
         return "redirect:/anotacao";
     }
 
-    @RequestMapping("/salvar")
+    @RequestMapping("anotacao/salvar")
     public String salvarTrabalho(Anotacao anotacao){
         repositorioAnotacao.save(anotacao);
         return "redirect:/anotacao";
